880141ad9fcafef04b06e809e642b40405909579
JayvonK <jaykhuth@gmail.com>
2024-02-04 03:03:24 -0800

    fix

--- js/question4.js
+++ js/question4.js
@@ -149,7 +149,7 @@ btnDark.addEventListener('click', (event) => {
     nav.className = "navbar navbar-light bg-dark2";
     text1.className = "light";
     text2.className = "d-flex justify-content-end light";
-    box.className = "col-sm-10 bg-dark1 boxHeight";
+    box.className = "col-12 col-lg-10 bg-dark1 boxHeight";
     innerBox.className = "col-12 dashSection bg-dark2 roundedCorner alpha-4 light";
     innerBox2.className = "col-12 dashSection bg-dark2 roundedCorner alpha-4 light";
 })
@@ -158,7 +158,7 @@ btnLight.addEventListener('click', (event) => {
     nav.className = "navbar navbar-light bg-light";
     text1.className = "dark";
     text2.className = "d-flex justify-content-end dark";
-    box.className = "col-sm-10 lightGreyBg boxHeight";
+    box.className = "col-12 col-lg-10 lightGreyBg boxHeight";
     innerBox.className = "col-12 dashSection bg-white roundedCorner alpha-4";
     innerBox2.className = "col-12 dashSection bg-white roundedCorner alpha-4";
 })
--- js/question5.js
+++ js/question5.js
@@ -11,7 +11,7 @@ btnDark.addEventListener('click', (event) => {
     nav.className = "navbar navbar-light bg-dark2";
     text1.className = "light";
     text2.className = "d-flex justify-content-end light";
-    box.className = "col-sm-10 bg-dark1 boxHeight";
+    box.className = "col-12 col-lg-10 bg-dark1 boxHeight";
     innerBox.className = "col-12 dashSection bg-dark2 roundedCorner alpha-4 light";
 })
 
@@ -19,6 +19,6 @@ btnLight.addEventListener('click', (event) => {
     nav.className = "navbar navbar-light bg-light";
     text1.className = "dark";
     text2.className = "d-flex justify-content-end dark";
-    box.className = "col-sm-10 lightGreyBg boxHeight";
+    box.className = "col-12 col-lg-10 lightGreyBg boxHeight";
     innerBox.className = "col-12 dashSection bg-white roundedCorner alpha-4";
 })
