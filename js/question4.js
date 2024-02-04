202b9349c589fe5847bfdb580c55fbd3ca43223b
JayvonK <jaykhuth@gmail.com>
2024-02-04 02:38:18 -0800

    q4 comments

--- js/question4.js
+++ js/question4.js
@@ -70,33 +70,7 @@ randomCSSBtn.addEventListener('click', (event) => {
     result.className = `${wickedArr[index]} simple-box pt-5`;
 })
 
-let nav = document.getElementById("nav");
-let text1 = document.getElementById("text1");
-let text2 = document.getElementById("text2");
-let box = document.getElementById("box");
-let btnLight = document.getElementById("btnLight");
-let btnDark = document.getElementById("btnDark");
-let innerBox = document.getElementById("innerBox");
-let innerBox2 = document.getElementById("innerBox2");
-
-btnDark.addEventListener('click', (event) => {
-    nav.className = "navbar navbar-light bg-dark2";
-    text1.className = "light";
-    text2.className = "d-flex justify-content-end light";
-    box.className = "col-sm-10 bg-dark1 boxHeight";
-    innerBox.className = "col-12 dashSection bg-dark2 roundedCorner alpha-4 light";
-    innerBox2.className = "col-12 dashSection bg-dark2 roundedCorner alpha-4 light";
-})
-
-btnLight.addEventListener('click', (event) => {
-    nav.className = "navbar navbar-light bg-light";
-    text1.className = "dark";
-    text2.className = "d-flex justify-content-end dark";
-    box.className = "col-sm-10 lightGreyBg boxHeight";
-    innerBox.className = "col-12 dashSection bg-white roundedCorner alpha-4";
-    innerBox2.className = "col-12 dashSection bg-white roundedCorner alpha-4";
-})
-
+//Again creating event listeners, that will change the box's animation class off of the user's click. Each event listener has different values of wicked css class names that they pass into the paremeter of the function.
 dropdown1.addEventListener('click', (event) => {
     ChangeClass("barrelRoll");
 })
@@ -159,4 +133,32 @@ dropdown20.addEventListener('click', (event) => {
 })
 dropdown21.addEventListener('click', (event) => {
     ChangeClass("zoomerOut");
+})
+
+//All this code below is just allowing the page to have a dark mode and light mode. Ability to switch colors on the page from a button click
+let nav = document.getElementById("nav");
+let text1 = document.getElementById("text1");
+let text2 = document.getElementById("text2");
+let box = document.getElementById("box");
+let btnLight = document.getElementById("btnLight");
+let btnDark = document.getElementById("btnDark");
+let innerBox = document.getElementById("innerBox");
+let innerBox2 = document.getElementById("innerBox2");
+
+btnDark.addEventListener('click', (event) => {
+    nav.className = "navbar navbar-light bg-dark2";
+    text1.className = "light";
+    text2.className = "d-flex justify-content-end light";
+    box.className = "col-sm-10 bg-dark1 boxHeight";
+    innerBox.className = "col-12 dashSection bg-dark2 roundedCorner alpha-4 light";
+    innerBox2.className = "col-12 dashSection bg-dark2 roundedCorner alpha-4 light";
+})
+
+btnLight.addEventListener('click', (event) => {
+    nav.className = "navbar navbar-light bg-light";
+    text1.className = "dark";
+    text2.className = "d-flex justify-content-end dark";
+    box.className = "col-sm-10 lightGreyBg boxHeight";
+    innerBox.className = "col-12 dashSection bg-white roundedCorner alpha-4";
+    innerBox2.className = "col-12 dashSection bg-white roundedCorner alpha-4";
 })
